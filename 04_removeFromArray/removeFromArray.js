8c4d3ce5473a4c1c48a8c096f340d1df1a8c81ab
Regis1272 <21206554+Regis1272@users.noreply.github.com>
2023-06-28 10:11:45 -0700

    Solution to 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  // A much less eloquent solution, to be sure...
+  let filteredArray = array;
+  array.forEach(element => {
+    for (let arg of args) {
+      filteredArray = filteredArray.filter(word => word !== arg);
+    }
+  });
+  return filteredArray;
 };
 
 // Do not edit below this line
