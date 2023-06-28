fa425929547c834941a8a9e131dd431177301e32
Regis1272 <21206554+Regis1272@users.noreply.github.com>
2023-06-28 12:05:59 -0700

    solution for 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  if (typeof num === "string") num = Number(num);
 
+  let fib = [];
+
+  for (let i = 0; i < num; i++) {
+    if (i <= 1) {
+      fib.push(1);
+      continue;
+    }
+    fib.push(fib[i-1] + fib[i-2]);
+  }
+  
+  return fib[num-1];
 };
 
 // Do not edit below this line
