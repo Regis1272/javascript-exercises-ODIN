6f5d0b7d5b1faf40f6c2d6c8f8a1a3698f4815ba
Regis1272 <21206554+Regis1272@users.noreply.github.com>
2023-06-28 09:23:11 -0700

    Solution to exercise 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let reversed = "";
+  for (let i = 0; i < word.length; i++) {
+    reversed += word[word.length - 1 - i];
+  }
+  return reversed;
 };
 
 // Do not edit below this line
