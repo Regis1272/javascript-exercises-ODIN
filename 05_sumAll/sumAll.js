4719d0550ab32fee4b55663b423f54b8b0b1de72
Regis1272 <21206554+Regis1272@users.noreply.github.com>
2023-06-28 10:34:07 -0700

    solution to 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+  if (num1 < 0 || num2 < 0) return "ERROR";
 
+  let total = 0;
+
+  for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
