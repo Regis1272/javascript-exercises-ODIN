76cd5923703b18ded877a99c00305716e96e4027
Regis1272 <21206554+Regis1272@users.noreply.github.com>
2023-06-28 11:30:46 -0700

    solution to 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let flatString = string.toLowerCase();
+  let filteredString = "";
+  for (letter of flatString) {
+    let asciiVal = letter.charCodeAt(0);
+    if ((asciiVal >= 48 && asciiVal <= 57) ||
+      (asciiVal >= 97 && asciiVal <= 122)) {
+      filteredString += letter;
+    }
+  }
+  let reverseString = filteredString.split("").reverse().join("");
+  
+  if (reverseString === filteredString) return true;
+  else return false;
 };
 
 // Do not edit below this line
